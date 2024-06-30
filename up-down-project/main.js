57986adf2cbfffa3690ea250539a556a7b681350
stephanie <j0102276@gmail.com>
2024-06-30 09:36:31 +0900

    first commit

---
+++ up-down-project/main.js
@@ -0,0 +1,38 @@
+//램덤번호 지정
+//유저가 번호를 입력한다. 그리고 go라는 버튼을 누름
+//만약에 유저가 램덤번호를 맞추면, 맞췄습니다!
+//랜덤 번호가 < 유저번호  DOWN
+//랜덤 번호가 < 유저번호  up
+//reset 버튼을 누르면 게임이 리셋된다
+//5번의 기회를 다쓰면 게임이 끝난다.(더이상 추측 불가, 버튼이 disable)
+//유저가 1~100 범위 밖에 숫자를 입력함변 알려준다. 기회를 깍지 않는다
+//유저가 이미 입력한 숫자를 또 입력하면, 기회를 깍지 않는다.
+
+
+let computerNum =0
+let playButton = document.getElementById("play-button")
+let userInput = document.getElementById("user-input")
+let resultArea = document.getElementById("result-area")
+
+
+playButton.addEventListener("click",play)
+
+
+function pickComputerNum(){
+    computerNum = Math.floor(Math.random()*100)+1;
+    console.log("정답 : ",computerNum);
+}
+pickComputerNum();
+
+
+function play(){
+    let userValue = userInput.value;
+    if(userValue < computerNum){
+        resultArea.textContent("up")
+    }else if(userValue > computerNum){
+        resultArea.textContent("down")
+    }else{
+        resultArea.textContent("맞추셨습니다")
+    }
+}
+
